ae1196bf731781d0a2c9d5d238cb6c20b5b949f8
root <root@ubuntu.(none)>
2014-04-26 08:48:22 -0700

    Calls monitor

--- ccmelog.py
+++ ccmelog.py
@@ -4,14 +4,20 @@ from twisted.internet import protocol
 import sys
 import MySQLdb
 
-
 class BroadcastingDatagramProtocol(protocol.DatagramProtocol):
-    db = MySQLdb.connect(host='localhost', 
-                         user='root', 
-                         passwd='qwe123',
-                         db='ccme_telephony',
-                         charset='utf8')
+    config = { 'host':'localhost', 
+               'user':'root', 
+               'passwd':'qwe123',
+               'db':'ccme_telephony',
+               'charset':'utf8' }
+    db = MySQLdb.connect(**config)
     db_session = []
+    sign = ('<190>','<189>')
+    def save(self, data):
+        if any(x in data for x in self.sign):
+            self.db_session.execute("""INSERT INTO ccme_raw(data) 
+                                       VALUES ('%s')""" % data)
+            self.db.commit()
 
     def startProtocol(self):
         self.db_session = self.db.cursor()
@@ -22,8 +28,11 @@ class BroadcastingDatagramProtocol(protocol.DatagramProtocol):
 
     def datagramReceived(self, data, addr):
         try:
-            self.db_session.execute("INSERT INTO ccme_raw(data) VALUES ('%s')" % data)
-            self.db.commit()
+            self.save(data)
+        except (AttributeError, MySQLdb.OperationalError):
+            self.db = MySQLdb.connect(**config)
+            self.db_session = self.db.cursor()
+            self.save(data)       
         except:
             self.db.rollback()
 
